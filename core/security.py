e9884cf7c0e2523c5a4c1e38f2769e4b6c916fd7
moriya-wsl <y.moriya.113@gmail.com>
2025-04-10 01:19:54 +0900

    int変換の削除。#2

--- core/security.py
+++ core/security.py
@@ -58,7 +58,6 @@ async def get_current_user(
         user_id_str = payload.get("sub")
         if user_id_str is None:
             raise credentials_exception
-        user_id = int(user_id_str)
     except (JWTError, ValueError):
         raise credentials_exception
 
